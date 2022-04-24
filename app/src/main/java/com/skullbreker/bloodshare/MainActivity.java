4fbef5e81b79ed656af279c307dc4461f6ccf049
Tafriyadi27 <87406088+Tafriyadi27@users.noreply.github.com>
2022-04-24 14:02:17 +0700

    Little Editing in MainActivity.java

--- app/src/main/java/com/skullbreker/bloodshare/MainActivity.java
+++ app/src/main/java/com/skullbreker/bloodshare/MainActivity.java
@@ -1,28 +1,27 @@
 package com.skullbreker.bloodshare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 // Tanggal Pengerjaan   : 22-April-2022
 // Deskripsi Pengerjaan : 22-April-2022
-//                       - Mengerjakan Layout activity_main(Welcome)
+//                       - Mengerjakan dan Mendesign Layout activity_main(Welcome)
 //                       - Membuat Class MainActivity(Welcome)
-//                       - Memasangkan logika pada Text "Register" agar bisa dijadikan sebagai tombol dan dapt pindah halaman dari MainActivity.java ke class RegisterActivity.java
-//                       - Mengerjakan Layout activity_register
+//                       - Memasangkan logika pada Text "Register" agar bisa dijadikan sebagai tombol dan dapt pindah class dari MainActivity.java ke class RegisterActivity.java
+//                       - Mengerjakan dan Mendesign Layout activity_register
 //                       - Membuat Class RegisterActivity
-//                       - Memasangkan logika pada Button "Register" agar dapat pindah halaman dari RegisterActivity.java ke class AlmostThereActivity.java
+//                       - Memasangkan logika pada Button "Register" agar dapat pindah class dari RegisterActivity.java ke class AlmostThereActivity.java
 //                        23-April-2022
-//                       - Mengerjakan Layout activity_almostthere
+//                       - Mengerjakan dan Mendesign Layout activity_almostthere
 //                       - Membuat Class AlmostThereActivity
-//                       - Memasangkan logika pada Button "Verify" agar dapat pindah halaman dari AlmostThereActivity.java ke class VerifyActivity.java
-//                       - Mengerjakan Layout activity_verify
+//                       - Memasangkan logika pada Button "Verify" agar dapat pindah class dari AlmostThereActivity.java ke class VerifyActivity.java
+//                       - Mengerjakan dan Mendesign Layout activity_verify
 //                       - Membuat Class VerifyActivity
-//                       - Memasangkan logika pada Button "Send" agar dapat pindah halaman dari VerifyActivity.java ke class HomeActivity.java
-//                       - Mengerjakan Layout activity_home
+//                       - Memasangkan logika pada Button "Send" agar dapat pindah class dari VerifyActivity.java ke class HomeActivity.java
+//                       - Mengerjakan dan Mendesign Layout activity_home
 //                       - Membuat Class HomeActivity
 // Nama                 : Tri Tafriyadi
 // NIM                  : 10119213
@@ -35,13 +34,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView=(TextView) findViewById(R.id.register);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
-                startActivity(intent);
-            }
+        textView= findViewById(R.id.register);
+        textView.setOnClickListener(v -> {
+            Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+            startActivity(intent);
         });
     }
 }
